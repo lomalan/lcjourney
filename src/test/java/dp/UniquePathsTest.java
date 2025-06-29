70d58c49c37ad6123319f27bcadd2d90edf51959
lomalan <lomalanio@gmail.com>
2025-06-29 14:26:52 +0100

    dp 2d started

---
+++ src/test/java/dp/UniquePathsTest.java
@@ -0,0 +1,19 @@
+package dp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UniquePathsTest {
+    private final UniquePaths uniquePaths = new UniquePaths();
+
+    @Test
+    void threeUniquePaths() {
+        assertEquals(3, uniquePaths.uniquePaths(3, 2));
+    }
+
+    @Test
+    void manyUniquePaths() {
+        assertEquals(28, uniquePaths.uniquePaths(3, 7));
+    }
+}
